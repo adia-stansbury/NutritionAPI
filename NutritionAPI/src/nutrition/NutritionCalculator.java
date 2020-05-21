1689467854064646e2928cbb387859d13e5f605a
Adia <Adia@ADIA_GAMING>
2020-05-20 20:06:13 -0400

    WIP: implementing streams to iterate through ingredients

--- NutritionAPI/src/nutrition/NutritionCalculator.java
+++ NutritionAPI/src/nutrition/NutritionCalculator.java
@@ -2,21 +2,24 @@ package nutrition;
 
 import java.util.EnumMap;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Hashtable;
 import java.util.Iterator;
 import java.util.Map;
+import java.util.Map.Entry;
 
 // Calculates nutrition of ingredients
 public class NutritionCalculator {
-	// { "1049": { gramWeight: 5 }}
-	HashMap<String, HashMap<String, Double>> ingredientsIntake;
+	// { "1049": { "gramWeight": 5 }, "1055: {"gramWeight": 50 }}
+	Hashtable<String, Hashtable<String, Double>> ingredients;
 	
-	NutritionCalculator(HashMap<String, HashMap<String, Double>> ingredientsIntake) {
-		this.ingredientsIntake = ingredientsIntake;
+	NutritionCalculator(Hashtable<String, Hashtable<String, Double>> ingredients) {
+		this.ingredients = ingredients;
 	}
 	
-	public EnumMap<Nutrient, HashMap<String, Double>> calculate() {
-		EnumMap<Nutrient, HashMap<String, Double>> nutrition = new EnumMap<Nutrient, HashMap<String, Double>>(Nutrient.class);
-		HashMap<String, Double> nutritionForZinc = new HashMap<String, Double>();
+	public EnumMap<Nutrient, Hashtable<String, Double>> calculate() {
+		EnumMap<Nutrient, Hashtable<String, Double>> nutrition = new EnumMap<Nutrient, Hashtable<String, Double>>(Nutrient.class);
+		Hashtable<String, Double> nutritionForZinc = new Hashtable<String, Double>();
 		nutritionForZinc.put("gramWeight", gramWeight(Nutrient.ZINC));
 		nutrition.put(Nutrient.ZINC, nutritionForZinc);
 		
@@ -24,10 +27,9 @@ public class NutritionCalculator {
 	}
 
 	private double gramWeight(Nutrient nutrient) {
-		Iterator ingredientIntakeIterator = ingredientIntakeIterator();
-		while (ingredientIntakeIterator.hasNext()) {
-			Map.Entry ingredientIntake = (Map.Entry)ingredientIntakeIterator.next(); // TODO: fix Map.Entry to include hashMap as value
-			nutrition((String) ingredientIntake.getKey(), (double) ingredientIntake.get("gramWeight").nutrient);
+		for(Hashtable<String, Hashtable<String, Double>> ingredient : ingredients) {
+			String ingredientID = ingredient.getKey();
+			nutrition(ingredientID, ingredientGramWeight.nutrient);
 			
 		}
 	}
@@ -35,11 +37,6 @@ public class NutritionCalculator {
 	private double nutrition(String ingredientID, double ingredientGramWeight, Nutrient nutrient) {
 		Ingredient ingredient = new Ingredient(ingredientID);
 		return ingredient.nutrientGramWeight(ingredientGramWeight, nutrient);	
-	}
-
-	private Iterator ingredientIntakeIterator() {
-		Iterator ingredientIntakeIterator = ingredientsIntake.entrySet().iterator();
-		return ingredientIntakeIterator;
 	}
 	
 	public static void main(String[] args) {
